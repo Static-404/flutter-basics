662074796e34f97e98648bcb9ca562ac92abfbbb
Static-404 <pboybams16@gmail.com>
2023-12-07 11:39:11 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 26, 2, 70),
+                Color.fromARGB(255, 44, 4, 118),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(fontSize: 28, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
